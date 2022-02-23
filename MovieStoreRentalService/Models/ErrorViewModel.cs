4e75c1cc0f90fa490b2a16ffaf9ab9989424fe00
Metody Aleksov <teddy57603@gmail.com>
2022-02-23 19:32:56 +0200

    Add project files.

---
+++ MovieStoreRentalService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieStoreRentalService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
