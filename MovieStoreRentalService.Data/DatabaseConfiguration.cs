1205a78c061a403d5cd4e3dd7a0d1dbc68cafb44
Metody Aleksov <teddy57603@gmail.com>
2022-02-26 18:37:40 +0200

    created database the database context

---
+++ MovieStoreRentalService.Data/DatabaseConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieStoreRentalService.Data;
+
+public static class DatabaseConfiguration
+{
+    public static string ConnectionString
+        => @"Server=.;Database=MovieStoreRental;Trusted_Connection=True;Integrated Security=True;";
+}
---
+++ MovieStoreRentalService.Data/MovieStoreRentalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieStoreRentalService.Data
+{
+    public class MovieStoreRentalDbContext : DbContext
+    {
+        public MovieStoreRentalDbContext()
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
