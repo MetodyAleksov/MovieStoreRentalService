05cafc6eefd851021736e4cdcd0f7ac62fcb0b14
Metody Aleksov <teddy57603@gmail.com>
2022-02-28 21:54:53 +0200

    added core project and added const messages

---
+++ MovieStoreRentalService.Core/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieStoreRentalService.Core
+{
+    public class Constants
+    {
+        public const string ErrorMessage = "ErrorMessage";
+        public const string WarningMessage = "WarningMessage";
+        public const string SuccessMessage = "SuccessMessage";
+    }
+}
