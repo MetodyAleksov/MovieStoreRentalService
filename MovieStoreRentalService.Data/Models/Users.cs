2d7fc466fc22d93663d6dedae89af1f354b59c61
Metody Aleksov <teddy57603@gmail.com>
2022-03-03 15:04:59 +0200

    Users now extends identityuser

--- MovieStoreRentalService.Data/Models/Users.cs
+++ MovieStoreRentalService.Data/Models/Users.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace MovieStoreRentalService.Data.Models;
 
-public class Users
+public class Users : IdentityUser
 {
     public Users()
     {
