2ae29c07114a1aee0d231dafefcdc54fec7b4586
Metody Aleksov <teddy57603@gmail.com>
2022-03-05 17:24:26 +0200

    added validation constants

---
+++ MovieStoreRentalService.Core/Common/ValidationConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieStoreRentalService.Core;
+
+public class ValidationConstants
+{
+    public const int RENTAL_NAME_MAX_L = 100;
+    public const int RENTAL_NAME_MIN_L = 2;
+
+    public const int RENTAL_IMAGEURL_MIN_L = 10;
+    public const int RENTAL_IMAGEURL_MAX_L = 2084;
+
+    public const int RENTAL_AMOUNTAVAILABLE_MIN = 2;
+    public const int RENTAL_AMOUNTAVAILABLE_MAX = 1000;
+
+    public const decimal RENTAL_PRICE_MIN = 2;
+    public const decimal RENTAL_PRICE_MAX = 100;
+}
