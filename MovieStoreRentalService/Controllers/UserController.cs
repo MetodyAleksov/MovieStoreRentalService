dffaaca065b6fe48238f52dced189f6899e4af27
Metody Aleksov <teddy57603@gmail.com>
2022-03-18 19:28:42 +0200

    added profile view and user roles

--- MovieStoreRentalService/Controllers/UserController.cs
+++ MovieStoreRentalService/Controllers/UserController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using MovieStoreRentalService.DTO;
 using MovieStoreRentalService.DTO.Common.Enums;
+using MovieStoreRentalService.Services.User;
 
 namespace MovieStoreRentalService.Controllers
 {
     public class UserController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUserService _userService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController
+            (RoleManager<IdentityRole> roleManager
+            , IUserService userService
+            , UserManager<ApplicationUser> userManager)
+        {
+            this._roleManager = roleManager;
+            _userManager = userManager;
+            _userService = userService;
+        }
+
         public IActionResult Library()
         {
             RentalDTO dto = new RentalDTO()
@@ -17,6 +34,32 @@ namespace MovieStoreRentalService.Controllers
             };
 
             return View(null, dto);
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = await _userService.GetAllUsers();
+
+            
+
+            return Ok();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> CreateRole()
+        {
+            //await _roleManager.CreateAsync(new IdentityRole()
+            //{
+            //    Name = "Administrator"
+            //});
+
+            return Ok();
         }
     }
 }
