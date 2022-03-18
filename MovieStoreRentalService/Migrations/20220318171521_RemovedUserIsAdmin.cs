9c20e95a04eef01abd18df03f297eaf7e1b54cdd
Metody Aleksov <teddy57603@gmail.com>
2022-03-18 19:27:39 +0200

    removed users isAdmin property

---
+++ MovieStoreRentalService/Migrations/20220318171521_RemovedUserIsAdmin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieStoreRentalService.Migrations
+{
+    public partial class RemovedUserIsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
