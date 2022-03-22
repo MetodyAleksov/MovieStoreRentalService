baddafea0a62386c72a009d41742ee0ff8f340fd
Metody Aleksov <teddy57603@gmail.com>
2022-03-22 18:55:13 +0200

    removed Admin area

--- MovieStoreRentalService.Services/User/IUserService.cs
+++ MovieStoreRentalService.Services/User/IUserService.cs
@@ -1,15 +1,9 @@
-﻿using MovieStoreRentalService.DTO.ViewModels;
-
-namespace MovieStoreRentalService.Services.User
+﻿namespace MovieStoreRentalService.Services.User
 {
     public interface IUserService
     {
         Task<IEnumerable<ApplicationUser>> GetAllUsers();
 
         Task<ApplicationUser> GetUserById(string id);
-
-        Task<UserEditViewModel> GetUserForEdit(string id);
-
-        Task<bool> UpdateUser(UserEditViewModel model);
     }
 }
--- MovieStoreRentalService.Services/User/UserService.cs
+++ MovieStoreRentalService.Services/User/UserService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MovieStoreRentalService.Data.Common;
-using MovieStoreRentalService.DTO.ViewModels;
 
 namespace MovieStoreRentalService.Services.User
 {
@@ -21,33 +20,6 @@ namespace MovieStoreRentalService.Services.User
         public async Task<ApplicationUser> GetUserById(string id)
         {
             return await _repo.All<ApplicationUser>().SingleOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<UserEditViewModel> GetUserForEdit(string id)
-        {
-            var user = await _repo.All<ApplicationUser>().FirstAsync(u => u.Id == id);
-
-            return new UserEditViewModel()
-            {
-                Id = user.Id,
-                Username = user.UserName
-            };
-        }
-
-        public async Task<bool> UpdateUser(UserEditViewModel model)
-        {
-            bool result = false;
-            var user = await _repo.All<ApplicationUser>().SingleAsync(u => u.Id == model.Id) ;
-
-            if (user != null)
-            {
-                user.UserName = model.Username;
-
-                await _repo.SaveChangesAsync();
-                result = true;
-            }
-
-            return result;
         }
     }
 }
