9e11fbb069647af47df2d8e18d3d67c923eb1980
Metody Aleksov <teddy57603@gmail.com>
2022-03-24 20:23:34 +0200

    added cartDTO

---
+++ MovieStoreRentalService.DTO/CartDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieStoreRentalService.DTO
+{
+    public class CartDTO
+    {
+        public string UserId { get; set; }
+
+        public string CartId { get; set; }
+
+        public ICollection<RentalDTO> Rentals { get; set; }
+    }
+}
